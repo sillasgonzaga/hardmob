1f4093f6e582bce6162d38168e048cd19935e8cb
Sillas Gonzaga <sillas.gonzaga@gmail.com>
2017-10-31 01:11:22 +0000

    try condition

--- R/scraping/02b-extrair_posts_funcao.R
+++ R/scraping/02b-extrair_posts_funcao.R
@@ -6,11 +6,13 @@ df_topicos <- readRDS("data/02-df_paginas_topicos.Rds")
 # rodar loop e salvar resultados
 urls <- unique(df_topicos$pgs_topico)
 
-for (i in 3205:length(urls)){
+for (i in 1:length(urls)){
   print(i)
   
   url_loop <- urls[i]
-  df_loop <- extrair_dados_posts(url_loop)
+  df_loop <- try(extrair_dados_posts(url_loop), silent = TRUE)
+  
+  if (inherits(df_loop, "try-error")) next
   
   ind_loop <- str_pad(i, width = 4, pad =  "0")
   arquivo <- paste0("data/02b/", ind_loop, ".Rds")
